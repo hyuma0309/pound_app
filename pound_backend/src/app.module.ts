c0419ab48a28de64074a3745b14a9452a821cc17
hyuma <sunanoeikan0309@gmail.com>
2024-12-01 10:02:10 +0900

    バックエンドsentryの追加

--- pound_backend/src/app.module.ts
+++ pound_backend/src/app.module.ts
@@ -1,16 +1,26 @@
 import { Module } from '@nestjs/common';
+import { SentryModule } from '@sentry/nestjs/setup';
+import { APP_FILTER } from '@nestjs/core';
+import { SentryGlobalFilter } from '@sentry/nestjs/setup';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
+    SentryModule.forRoot(),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true,
       playground: true
     }),
     UsersModule,
+  ],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: SentryGlobalFilter,
+    }
   ],
 })
 export class AppModule {}
--- pound_backend/src/users/users.service.ts
+++ pound_backend/src/users/users.service.ts
@@ -1,18 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import * as Sentry from "@sentry/nestjs";
 
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {}
 
   async findAll() {
-    return this.prisma.user.findMany({
-      select: {
-        id: true,
-        username: true,
-        email: true,
-        created_at: true,
-      },
-    });
+    try {
+      // テスト用エラー
+      // throw new Error("Sentry Test Error in Users Service");
+      
+      return await this.prisma.user.findMany({
+        select: {
+          id: true,
+          username: true,
+          email: true,
+          created_at: true,
+        },
+      });
+    } catch (error) {
+      // エラーに追加情報を付与できる
+      Sentry.captureException(error, {
+        extra: {
+          service: 'UsersService',
+          method: 'findAll',
+        }
+      });
+      throw error; // エラーを上位に伝播
+    }
   }
 }
