f8a2a0219ba8a8722061e16b4929bced583dd6c3
浅田飛雄馬 <40336182+hyuma0309@users.noreply.github.com>
2025-02-16 09:26:47 +0900

    Revert "Test1"

--- pound_front/src/component/test-1.js
+++
@@ -1,9 +0,0 @@
-// 文字列を反転させる関数
-const reverseString = (str) => {
-    return str.split('').reverse().join('');
-};
-
-// エクスポート
-export {
-    reverseString,
-};
--- pound_front/src/component/test1.js
+++
@@ -1,9 +0,0 @@
-// テスト用の簡単な計算関数
-const add = (a, b) => {
-    return a + b;
-};
-
-// エクスポート
-export {
-    add,
-};
